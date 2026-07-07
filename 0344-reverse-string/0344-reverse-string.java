ab742a11100422c10f3eef7119ec5ef533599fe6
sanyamsood85 <sanyamsood85@gmail.com>
2026-07-07 11:28:26 +0530

    Time: 0 ms (100%), Space: 48.5 MB (10.82%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int i=0;
+        int r=s.length-1;
+        while(i<=r){
+            char temp=s[i];
+            s[i]=s[r];
+            s[r]=temp;
+            i++;
+            r--;
+        }
+        
+    }
+}
